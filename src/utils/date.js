eba3ddc77f4f0e9c1f8b3884ed28ddc060b9a346
Mikolaj <mikolajdorosz@gmail.com>
2024-09-06 00:47:08 +0200

    Fixed displaying addingDate

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,8 +1,14 @@
 export const toLocaleISOString = (date) => {
     const splitted = date.split("T");
+    const year = splitted[0].split("-")[0];
+    const month = splitted[0].split("-")[1];
+    let day = parseInt(splitted[0].split("-")[2]);
     const time = splitted[1].split(":");
     const hour = (parseInt(time[0]) + 2) % 24;
-    return `${splitted[0]}T${hour}:${time[1]}`;
+    if (hour >= 0 && hour < 2) day++;
+    return `${year}-${month}-${day.toString().padStart(2, "0")}T${hour
+        .toString()
+        .padStart(2, "0")}:${time[1]}`;
 };
 
 export const diffBetweenDates = (date1, date2, t1, t2, t3, t4) => {
